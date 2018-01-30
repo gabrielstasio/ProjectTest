23820405adccd9325cbc8c4ae2549eb0c06467a9
MUXICORP\gstasio <gstasio@muxi.com.br>
2018-01-30 10:34:18 -0200

    Initial Commit

---
+++ src/main/java/br/com/gsc/testProject/Boot.java
@@ -0,0 +1,30 @@
+package br.com.gsc.testProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class Boot
+{
+
+   public static void main(String[] args)
+   {
+
+      SpringApplication.run(Boot.class, args);
+      System.out.println("Application is running.");
+      Boot boot = new Boot();
+      boot.home();
+   }
+
+   @GetMapping("/")
+   @ResponseBody
+   public String home()
+   {
+      System.out.println("Called home.");
+      return "home";
+   }
+}
